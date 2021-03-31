92b7e977a811887ba3d2885bfaf86524e19f45e5
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-03-30 21:46:53 -0400

    first try

--- Week 2/3-30-21/merge.py
+++ Week 2/3-30-21/merge.py
@@ -1,5 +1,23 @@
+'''
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+'''
+
+
 class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+    def merge(nums1, m, nums2, n):
         """
         :type nums1: List[int]
         :type m: int
@@ -7,6 +25,14 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+
+        # get it up to mth num in nums1
+        return sorted(nums1[:m] + nums2[:n])
+
+        # get it up to nth num in nums2
+        # return nums2[:n]
+
+
         
     
 

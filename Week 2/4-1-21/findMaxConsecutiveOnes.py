20442e620bfb37080fc836da9e60f9a9d3f941e2
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-01 21:33:52 -0400

    1 approach

--- Week 2/4-1-21/findMaxConsecutiveOnes.py
+++ Week 2/4-1-21/findMaxConsecutiveOnes.py
@@ -16,11 +16,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        counter = 0
+        ans = []
 
+        # iterate the list
+        for num in nums:
+            # check if num in nums is 1
+            if num == 1:
+                # if it is counter += 1
+                counter += 1
+            # if it is not append counter to an empty and counter becomes 0
+            else:
+                ans.append(counter)
+                counter = 0
 
-
-
+        # return maximum number in the list
+        return max(ans)
 
     nums = [1,1,0,1,1,1]
     print(findMaxConsecutiveOnes(nums))

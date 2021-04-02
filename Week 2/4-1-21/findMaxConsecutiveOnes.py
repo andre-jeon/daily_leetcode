f75542a2ea597190316dc01ac9658b574caf394d
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-01 21:45:42 -0400

    done fast and furiously

--- Week 2/4-1-21/findMaxConsecutiveOnes.py
+++ Week 2/4-1-21/findMaxConsecutiveOnes.py
@@ -16,22 +16,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        counter = 0
-        ans = []
+        # counter = 0
+        # ans = []
 
-        # iterate the list
-        for num in nums:
-            # check if num in nums is 1
-            if num == 1:
-                # if it is counter += 1
-                counter += 1
-            # if it is not append counter to an empty and counter becomes 0
+        # # iterate the list
+        # for num in nums:
+        #     # check if num in nums is 1
+        #     if num == 1:
+        #         # if it is counter += 1
+        #         counter += 1
+        #     # if it is not append counter to an empty and counter becomes 0
+        #     else:
+        #         ans.append(counter)
+        #         counter = 0
+        
+        # ans.append(counter)
+
+        # # return maximum number in the list
+        # return max(ans)
+
+# Leetcode answer
+        
+        count = maxCount = 0
+        
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
             else:
-                ans.append(counter)
-                counter = 0
-
-        # return maximum number in the list
-        return ans
+                maxCount = max(count, maxCount)
+                count = 0
+                
+        return max(count, maxCount)
 
     nums = [1,1,0,1,1,1]
     print(findMaxConsecutiveOnes(nums))

24c8b15a2ac19fc05f6bdc29d5f3c787c2de2918
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-01 21:09:37 -0400

    question 1 start

---
+++ Week 2/4-1-21/findMaxConsecutiveOnes.py
@@ -0,0 +1,26 @@
+'''
+Given a binary array, find the maximum number of consecutive 1s in this array.
+
+Example 1:
+Input: [1,1,0,1,1,1]
+Output: 3
+
+Explanation: The first two digits or the last three digits are consecutive 1s.
+    The maximum number of consecutive 1s is 3.
+'''
+
+
+class Solution(object):
+    def findMaxConsecutiveOnes(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+
+
+
+
+
+    nums = [1,1,0,1,1,1]
+    print(findMaxConsecutiveOnes(nums))

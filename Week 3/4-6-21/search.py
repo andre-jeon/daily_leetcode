7889050b2b208066f8a2d85b8b01da37b138489f
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-06 21:53:27 -0400

    while loop setup complete

--- Week 3/4-6-21/search.py
+++ Week 3/4-6-21/search.py
@@ -17,11 +17,35 @@ Explanation: 2 does not exist in nums so return -1
 
 
 class Solution(object):
-    def search(self, nums, target):
+    def search(nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        
+
+        # create variables that records the high and the low (index-wise)
+        # these variables will change depending on where the guessing number lands
+        low = 0
+        high = len(nums) - 1
+
+        # create a while loop
+        # that goes until low and high become equal
+        while high >= low:
+            continue
+
+
+
+
+        # if target is not in nums return -1
+        return -1
+
+    
+
+    nums = [-1,0,3,5,9,12]
+    target = 9
+    
+    print(search(nums, target))
+
+
         

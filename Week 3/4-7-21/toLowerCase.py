3679c6c597362c6ef0e28f094fdd4919f5d824ba
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-07 21:18:28 -0400

    done

--- Week 3/4-7-21/toLowerCase.py
+++ Week 3/4-7-21/toLowerCase.py
@@ -24,14 +24,33 @@ class Solution(object):
         :rtype: str
         """
         # built in approah
-        return str.lower()
+        # return str.lower()
 
         # hashmap with zip approach
         upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         lower = "abcdefghijklmnopqrstuvwxyz"
         h = dict(zip(upper, lower))
+
+        # h = {A : a, B : b...}
         
-        return ''.join([h[x] if x in h else x for x in str])
+        ans = ''
+
+        for x in str:
+
+            # if x is capital letter
+            if x in h:
+                # then add the lowercase version to ans
+                ans += h[x]
+            
+            # if x is lowercase
+            else:
+                # then just add x to ans
+                ans += x
+        
+        return ans
+
+        # 1 liner code of above code
+        # return ''.join([h[x] if x in h else x for x in str])
 
     str = "Hello"
     print(toLowerCase(str))

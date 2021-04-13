1c8a3c8da9523930cfa2de384723debbc876cf10
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-12 21:09:13 -0400

    starting

--- Week 4/temp_name.py
+++ Week 4/temp_name.py
@@ -1,7 +1,31 @@
+'''
+Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+Example 1:
+
+Input: "ab-cd"
+Output: "dc-ba"
+
+Example 2:
+
+Input: "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+
+Example 3:
+
+Input: "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+'''
+
+
+
+
+
 class Solution(object):
-    def reverseOnlyLetters(self, S):
+    def reverseOnlyLetters(S):
         """
         :type S: str
         :rtype: str
         """
+        
         

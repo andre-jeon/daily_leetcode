909e652224e93600b43182aba788c44101ef0f78
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-13 22:00:42 -0400

    done

--- Week 4/4-13-21/isAnagram.py
+++ Week 4/4-13-21/isAnagram.py
@@ -16,10 +16,46 @@ Output: false
 
 
 class Solution(object):
-    def isAnagram(self, s, t):
+    def isAnagram(s, t):
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
+        # my attempt
+        # check if the length of s and t is equal
+        if len(s) != len(t):
+            # if it is not return False
+            return False
+
+        # sort both s and t and check if they are equal
+        if sorted(s) == sorted(t):
+            return True
+        return False
+
+        # hashmap approach
+        dictionary = {}
         
+        for i in s:
+            if i in dictionary:
+                dictionary[i] += 1
+            else:
+                dictionary[i] = 1
+
+        for i in t:
+            if i in dictionary:
+                dictionary[i] -= 1
+            else:
+                return False
+
+        for val in dictionary.values():
+            if val != 0:
+                return False
+        
+        return True
+    
+    s = "rat"
+    t = "car"
+
+    print(isAnagram(s, t))
+

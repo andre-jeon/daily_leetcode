386d942fb60964ec41c4231e155ce7f4380d8ee5
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-15 22:37:10 -0400

    done with this shit

--- Week 4/4-15-21/temp.py
+++ Week 4/4-15-21/temp.py
@@ -1,43 +1,44 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        # translations = {
-        #     "I": 1,
-        #     "V": 5,
-        #     "X": 10,
-        #     "L": 50,
-        #     "C": 100,
-        #     "D": 500,
-        #     "M": 1000
-        # }
+    def romanToInt(s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
 
-        # number = 0
+        number = 0
+
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        for char in s:
+            number += translations[char]
+        return number
 
         # s = s.replace("IV", "IIII").replace("IX", "VIIII")
         # s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
         # s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
-
-        # for char in s:
-        #     number += translations[char]
-        # return number
-
-        s = s.replace("IV", "IIII").replace("IX", "VIIII")
-        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
-        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
-        s = list(s)
-        ans = 0
-        for i in range(len(s)):
-            if s[i] == "I":
-                ans += 1
-            elif s[i] == "V":
-                ans += 5
-            elif s[i] == "X":
-                ans += 10
-            elif s[i] == "L":
-                ans += 50
-            elif s[i] == "C":
-                ans += 100
-            elif s[i] == "D":
-                ans += 500
-            elif s[i] == "M":
-                ans += 1000
-        return ans
+        # s = list(s)
+        # ans = 0
+        # for i in range(len(s)):
+        #     if s[i] == "I":
+        #         ans += 1
+        #     elif s[i] == "V":
+        #         ans += 5
+        #     elif s[i] == "X":
+        #         ans += 10
+        #     elif s[i] == "L":
+        #         ans += 50
+        #     elif s[i] == "C":
+        #         ans += 100
+        #     elif s[i] == "D":
+        #         ans += 500
+        #     elif s[i] == "M":
+        #         ans += 1000
+        # return ans
+    
+    s = "MCMXCIV"
+    print(romanToInt(s))

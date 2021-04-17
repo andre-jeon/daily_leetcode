4aea26c680eabf89f4b9dd3b5bfda0b1284e93dc
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-04-17 00:25:15 -0400

    im done

--- Week 4/4-16-21/temp.py
+++ Week 4/4-16-21/temp.py
@@ -1,40 +1,47 @@
 '''
-Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
 
-Each letter in the magazine string can only be used once in your ransom note.
+Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+
+ 
 
 Example 1:
 
-Input: ransomNote = "a", magazine = "b"
-Output: false
+Input: g = [1,2,3], s = [1,1]
+Output: 1
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+You need to output 1.
 
 Example 2:
 
-Input: ransomNote = "aa", magazine = "ab"
-Output: false
-
-Example 3:
-
-Input: ransomNote = "aa", magazine = "aab"
-Output: true
+Input: g = [1,2], s = [1,2,3]
+Output: 2
+Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2. 
+You have 3 cookies and their sizes are big enough to gratify all of the children, 
+You need to output 2.
 '''
 
 
 
 
 class Solution(object):
-    def canConstruct(ransomNote: str, magazine: str) -> str:
+    def findContentChildren(g, s):
         """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
         """
+        counter = 0
+
+        # iterate s to see if i is in g
+        for i in s:
+            # if it is then add 1 to counter and replace with 'full'
+            if i in g:
+                counter += 1
+
+            # if it isn't
+            else:
+                # return counter
+                return counter
         
-        # we are checking to see if ransonNote is in magazine
-
-
-    
-    ransomNote = "aa"
-    magazine = "aab"
-
-    print(canConstruct(ransomNote, magazine))

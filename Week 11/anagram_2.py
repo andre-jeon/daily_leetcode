4208f8964a5442b51a83244a1e4d180d309635bd
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-06-08 22:43:34 -0400

    solution 2

--- Week 11/anagram_2.py
+++ Week 11/anagram_2.py
@@ -1,12 +1,44 @@
+'''
+--- Directions
+    Check to see if two provided strings are anagrams of eachother.
+    One string is an anagram of another if it uses the same characters
+    in the same quantity. Only consider characters, not spaces
+    or punctuation.  Consider capital letters to be the same as lower case
+
+--- Examples
+    anagrams('rail safety', 'fairy tales') --> True
+    anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+    anagrams('Hi there', 'Bye there') --> False
+'''
 class Solution:
-    def chunk(array, size):
-        
-        final = [array[i * size:(i + 1) * size] for i in range((len(array) + size - 1) // size )]
+    def anagram_2(a, b):
 
-        return final
+        def toDic(str):
+            str = str.lower()
+            arr = []
+            mapping = {}
+            for a in str:
+                if a.isalpha():
+                    arr.append(a)
+                continue
+            for i in sorted(arr):
+                if i not in mapping:
+                    mapping[i] = 1
+                else:
+                    mapping[i] += 1
+            return mapping
+
+        if len(toDic(a).keys()) != len(toDic(b).keys()):
+            return False
+        for k in toDic(a).keys():
+            if toDic(a)[k] == toDic(b)[k]:
+                continue
+            else:
+                return False
+        return True
 
 
-    array = [1, 2, 3, 4]
-    size = 2
+    a = 'RAIL! SAFETY!'
+    b = 'fairy tales'
 
-    print(chunk(array, size))
+    print(anagram_2(a, b))
